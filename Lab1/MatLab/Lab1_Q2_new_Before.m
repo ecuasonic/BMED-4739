002a355ba24b9f20b516ffb5a77ae856dea14115
Richard Barrezueta <ecuasonic@gmail.com>
2024-01-30 14:03:42 -0500

    Update Lab1_Q2_new_Before.m

--- Lab1/MatLab/Lab1_Q2_new_Before.m
+++ Lab1/MatLab/Lab1_Q2_new_Before.m
@@ -6,12 +6,16 @@ clear;
 
 s = serial("COM3", 'BaudRate', 9600);
 
-rpm = 25; % constant
-ardTime = 0.6; % constant
+RPM = 25;
+
+OMEGA_DEG = (RPM / 60) * 180; 
+OMEGA_RAD = (RPM / 60) * 2 * pi;
+
+% wait() in the ardiuno code
+ARD_TIME = 0.6;
 
 t = 0;
 theta = 0;
-omega = rpm / 60;
 
 try
     fopen(s);
@@ -22,11 +26,12 @@ try
         fprintf(s, '%d\n',  theta);
         disp(theta);
 
-        time = (60 / rpm) * (abs(theta) / 360);
+        % waits for motor to finish before sending next motion
+        time = abs(theta) / OMEGA_DEG;
         pause(time + ardTime);
         
-        t = t + 0.5;
-        theta = floor(180*sin(omega*t));
+        t = t + 0.1;
+        theta = floor(180*sin(OMEGA_RAD*t));
     end
     
     fclose(s);
