bdddcf1742d4abe0b37e7aa892bd03bcc2ce2415
Richard Barrezueta <ecuasonic@gmail.com>
2024-01-30 13:48:49 -0500

    Update Lab1_Q2_new_After.m

--- Lab1/MatLab/Lab1_Q2_new_After.m
+++ Lab1/MatLab/Lab1_Q2_new_After.m
@@ -1,31 +1,47 @@
+% disconnects COM3 before attempting
 if ~isempty(instrfindall)
     fclose(instrfind);
     delete(instrfind);
 end
 clear;
 
+% connects to COM3
 s = serial("COM3", 'BaudRate', 9600);
 
-rpm = 30; % constant
-ardTime = 0.5; % constant
-omega = (rpm / 60)*2*pi; % constant (rad)
+% same rpm used in Arduino code
+% used to calculate time for motor to finish
+RPM = 30;
+ARD_TIME = 0.5;
+
+% degrees
+degA = 180;
+
+% radians
+OMEGA = (rpm / 60)*2*pi;
+A = degA * (pi/180);
 
 t = 0;
-degA = 180; % deg
-A = degA * (pi/180); % rad
 
 try
     fopen(s);
-    pause(2);
+
+    % waits for everything to catch up, just in case
+    pause(2); 
+
+    % time it takes for motor to finish
     time = A / omega;
 
     while true
 
+        % sends data to Arduino through serial
         fprintf(s, '%d\n',  degA);
         disp(degA);
 
+        % waits for motor to finish
         pause(time + ardTime);
-        
+
+        % flips the angle
+        % to simulate sinusoidal motion
         degA = -degA;
     end
     
