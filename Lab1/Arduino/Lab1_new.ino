676acdd92363e576ee0f7ed11dfa3e5a721d8fd9
Richard Barrezueta <ecuasonic@gmail.com>
2024-01-30 13:43:00 -0500

    Update Lab1_new.ino

--- Lab1/Arduino/Lab1_new.ino
+++ Lab1/Arduino/Lab1_new.ino
@@ -10,7 +10,7 @@ A4988 stepper(MOTOR_STEPS, DIR, STEP);
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   while (Serial.available() > 0) {
-    Serial.read(); // clears the serial buffer
+    Serial.read(); // clears the serial buffer before starting, just in case
   }
   Serial.println();
   stepper.begin(30,1); // 30 is the rpm, 1 is the microstep
@@ -20,24 +20,23 @@ void setup() {
 void loop() {
   static bool waitingForInput = true;
 
+  // waits for input in general, either directly from serial or through MatLab
   if (waitingForInput && Serial.available() > 0) {
     float angle = Serial.parseFloat();
+    // this part will not be seen if using through MatLab
     if (angle != 0) {
       Serial.print("Rotating ");
       Serial.print(angle);
       Serial.println(" Degrees.");
-      rotateStepper(angle);
+      stepper.rotate(angle);
       waitingForInput = false;
     }
   }
 
+  // waits for the motor to finish, in order to free itself for another input
   if (!waitingForInput && stepper.getStepsRemaining() == 0) {
     delay(500);
     Serial.println("Enter another rotation angle in degrees:");
     waitingForInput = true;
   }
-}
-
-void rotateStepper(int angle) {
-  stepper.rotate(angle);
 }
